00a567df56f0c604d38dd6045905b4f331602296
Brian Hill <brian.l.hill11@gmail.com>
2018-08-14 12:20:31 -0700

    Added code for merging Log R Ratio values from multiple BPMRecords

--- LogRRatioFormat.py
+++ LogRRatioFormat.py
@@ -1,4 +1,5 @@
 from vcf.parser import _Format
+import numpy as np
 
 
 class LogRRatioFormat(object):
@@ -31,6 +32,37 @@ class LogRRatioFormat(object):
         Returns:
             float: log R ratio
         """
-        # TODO: we need to handle the case where there are multiple BPMs
-        idx = bpm_records[0].index_num
-        return self._log_r_ratio[idx]
+        # if we have more than 1 BPMRecord, need to merge
+        log_r_ratio_list = []
+        if len(bpm_records) > 1:
+            # if we have more than 1 alt allele, return missing
+            if len(vcf_record.ALT) > 1:
+                return "."
+            for i in range(len(bpm_records)):
+                idx = bpm_records[i].index_num
+                log_r_ratio = self._log_r_ratio[idx]
+                log_r_ratio_list.append(log_r_ratio)
+
+                # snp = bpm_records[i].snp
+                # strand = bpm_records[i].ref_strand
+                # ref_allele = vcf_record.REF
+
+                # chrom = bpm_records[i].chromosome
+                # start_pos = bpm_records[i].pos
+                # alt_allele = vcf_record.ALT
+                # print("chr: {} pos: {} ref: {} alt: {} snp: {} strand: {} LRR: {}".format(chrom, start_pos, ref_allele, alt_allele, snp, strand, log_r_ratio))
+
+        # otherwise single BPMRecord
+        else:
+            # if we have a multi-allelic site, return missing
+            if len(vcf_record.ALT) > 1:
+                return "."
+            idx = bpm_records[0].index_num
+            log_r_ratio_list.append(self._log_r_ratio[idx])
+
+        # nanmedian ignores NaN values
+        final_log_r_ratio = np.nanmedian(log_r_ratio_list)
+        if np.isnan(final_log_r_ratio):
+            return "."
+        else:
+            return final_log_r_ratio
